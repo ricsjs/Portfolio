d99d72bc8e19df7212015a584abe5f6fb12a4a2a
Ricardo Alencar <ricfilho0007@gmail.com>
2023-04-25 11:39:39 -0300

    Criação no Header e primeira parte da Home

--- src/components/pages/Ricardo.js
+++ src/components/pages/Ricardo.js
@@ -1,7 +1,21 @@
+import './Ricardo.css'
+
+import logo from '../../assets/r.png'
+
+import NavBar from '../layout/NavBar';
+
 function Ricardo(){
     return(
-        <div>
-            página inicial
+        <div className="bodyContainer">
+            <NavBar src={logo} alt='logo'/>
+            <div className='linhaVertical'></div>
+            <h1 className='welcome'>Bem-vindo.</h1>
+            <h4 className='span'>
+                Sou desenvolvedor web Full-Stack, com foco atual em Front-End<br/>
+                utilizando a biblioteca ReactJS. Diariamente interagindo com a comunidade,<br/>
+                trocando conhecimento sobre o mercado e suas tecnologias, trabalhando em equipe<br/>
+                e utilizando metodologias ágeis para criar e manter aplicações web.
+            </h4>
         </div>
     )
 }
