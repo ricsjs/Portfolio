a40cf02b87153d5b8a205a0d9c0ccbf63ebdd6ed
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-29 10:41:01 -0300

    correções de texto

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -19,7 +19,7 @@ function Experience() {
                     funcionalidades em sites de grande alcance, como o da Lincoln Electric, uma
                     multinacional especializada no desenvolvimento, fabricação e
                     comercialização de produtos relacionados à soldagem e corte, sediada nos
-                    Estados Unidos
+                    Estados Unidos.
                 </span>
             </div>
 
@@ -29,13 +29,23 @@ function Experience() {
 
             <div className='experienceDesc'>
                 <span>
-                    Desenvolvi e entreguei com sucesso sites responsivos para algumas
-                    empresas clientes da agência, como a Brasil Park, uma das maiores redes de
-                    estacionamento do país. Além disso, prestei suporte e desenvolvi
-                    funcionalidades em sites de grande alcance, como o da Lincoln Electric, uma
-                    multinacional especializada no desenvolvimento, fabricação e
-                    comercialização de produtos relacionados à soldagem e corte, sediada nos
-                    Estados Unidos
+                    Como fundador da Evolve, trabalhei diretamente com os clientes, entendendo
+                    suas necessidades e objetivos de negócio. Desenvolvi sites personalizados
+                    em WordPress que atendiam às suas demandas, resultando em um alto nível
+                    de satisfação.
+                </span>
+            </div>
+
+            <div className='experienceDesc'>
+                <span>
+                    Ao longo dessas experiências, utilizei diversas tecnologias e frameworks, como
+                    PHP, HTML, CSS, MySQL, JavaScript, ReactJS e WordPress, para criar soluções web
+                    eficientes e de alta qualidade. Além disso, busquei aprimorar a experiência do
+                    usuário, garantir a entrega dentro dos prazos e obter resultados mensuráveis, como
+                    o aumento de tráfego, vendas e satisfação do cliente.
+                    Estou entusiasmado em trazer minhas habilidades e experiência para um novo
+                    desafio, onde possa contribuir com minha expertise em desenvolvimento web e
+                    agregar valor à equipe
                 </span>
             </div>
 
